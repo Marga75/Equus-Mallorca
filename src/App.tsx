b56fbc5a7a3e327a74d00ff0c482e456ed7e2b62
Marga <marga.llompart.75@gmail.com>
2025-02-08 18:33:16 +0100

    Todo funciona sin errores

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ import Index from "./paginas/Index";
 import Salto from './paginas/Salto';
 import Doma from './paginas/Doma';
 import Carreras from './paginas/Carreras';
-/*
 import Volteo from './paginas/Volteo';
 import DomaVaquera from './paginas/DomaVaquera';
 import Western from './paginas/Western';
+/*
 import Contacto from './paginas/Contacto';
 import CalculadoraExperiencia from './paginas/CalculadoraExperiencia';
 */
@@ -27,10 +27,10 @@ function App() {
             <Route path="/salto" element={<Salto />} />
             <Route path="/doma" element={<Doma />} />
             <Route path="/carreras" element={<Carreras />} />
-            {/*
             <Route path="/volteo" element={<Volteo />} />
             <Route path="/domaVaquera" element={<DomaVaquera />} />
             <Route path="/western" element={<Western />} />
+            {/*
             <Route
               path="/calculadoraExperiencia"
               element={<CalculadoraExperiencia />}
