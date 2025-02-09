994b239a9c0cd1256df52feb229d446ad004a602
Marga <marga.llompart.75@gmail.com>
2025-02-09 17:58:35 +0100

    Trabajo 100% terminado

--- src/componentes/Header.tsx
+++ src/componentes/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
       <div className="bg-[rgb(73,36,1)] text-center">
         <h1 
         data-key="titulo" 
-        className="p-5 text-cyan-50 uppercase underline text-[5em] text-shadow-lg"
+        className="p-5 text-cyan-50 uppercase underline text-[5em] text-shadow-lg font-bold"
         style={{ textShadow: "8px 7px 5px black" }}
         >Equitación</h1>
         <h2 data-key="subtitulo1" className="p-5 text-cyan-50 text-3xl font-bold">Todas las disciplinas que se practican en Mallorca</h2>
